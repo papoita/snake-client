2b9e04ac0ad2ad86768cf7ea2a6282f4641d3940
PaolaP <paolaperez8@gmail.com>
2021-11-15 23:57:27 -0500

    module.export and destructure

--- play.js
+++ play.js
@@ -2,22 +2,9 @@
 
 const net = require("net");
 
+const { connect } = require("./client");
+
 // establishes a connection with the game server
-const connect = function () {
-	const conn = net.createConnection({
-		host: "localhost",
-		port: 50541,
-	});
-
-	// interpret incoming data as text
-	conn.setEncoding("utf8");
-
-	conn.on("connect", () => {
-		console.log("Server says: you idled!");
-	});
-
-	return conn;
-};
 
 console.log("Connecting ...");
 connect();
