091a215e5544ca83982ffddb569aee9f68111333
PaolaP <paolaperez8@gmail.com>
2021-11-15 23:11:18 -0500

    connection with repository for multiplayer game successfull

---
+++ play.js
@@ -0,0 +1,23 @@
+/** @format */
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+	const conn = net.createConnection({
+		host: "localhost",
+		port: 50541,
+	});
+
+	// interpret incoming data as text
+	conn.setEncoding("utf8");
+
+	conn.on("connect", () => {
+		console.log("Server says: you idled!");
+	});
+
+	return conn;
+};
+
+console.log("Connecting ...");
+connect();
