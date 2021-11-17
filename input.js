e1d53723490164b2ee2ae52c0ce4c4952125393c
PaolaP <paolaperez8@gmail.com>
2021-11-16 19:55:04 -0500

    refactor code to input

---
+++ input.js
@@ -0,0 +1,31 @@
+/** @format */
+
+const setupInput = function () {
+	const stdin = process.stdin;
+	stdin.setRawMode(true);
+	stdin.setEncoding("utf8");
+	stdin.resume();
+	stdin.on("data", handleUserInput);
+	return stdin;
+};
+
+const handleUserInput = function (key) {
+	if (key == "\u001B\u005B\u0041") {
+		process.stdout.write("up");
+	}
+	if (key == "\u001B\u005B\u0043") {
+		process.stdout.write("right");
+	}
+	if (key == "\u001B\u005B\u0042") {
+		process.stdout.write("down");
+	}
+	if (key == "\u001B\u005B\u0044") {
+		process.stdout.write("left");
+	}
+	// \u0003 maps to ctrl+c input
+	if (key == "\u0003") {
+		process.exit();
+	}
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,39 +2,11 @@
 
 const net = require("net");
 const { connect } = require("./client");
-
+const { setupInput } = require("./input");
 // establishes a connection with the game server
-const setupInput = function () {
-	const stdin = process.stdin;
-	stdin.setRawMode(true);
-	stdin.setEncoding("utf8");
-	stdin.resume();
-	stdin.on("data", handleUserInput);
-	return stdin;
-};
-
-const handleUserInput = function (key) {
-	if (key == "\u001B\u005B\u0041") {
-		process.stdout.write("up");
-	}
-	if (key == "\u001B\u005B\u0043") {
-		process.stdout.write("right");
-	}
-	if (key == "\u001B\u005B\u0042") {
-		process.stdout.write("down");
-	}
-	if (key == "\u001B\u005B\u0044") {
-		process.stdout.write("left");
-	}
-	// \u0003 maps to ctrl+c input
-	if (key == "\u0003") {
-		process.exit();
-	}
-};
-
-setupInput();
 
 console.log("Connecting ...");
 connect();
 
+setupInput();
 module.exports = { setupInput };
